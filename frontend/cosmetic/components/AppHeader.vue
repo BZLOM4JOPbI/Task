f537a029c1cbd4a2e542ca42d4f938c73ccfdfa5
BZLOM4JOPbI <prostoyachelovek@gmail.com>
2023-04-12 00:22:22 +0300

    Catalogue page, default styles and layout

--- frontend/cosmetic/components/AppHeader.vue
+++ frontend/cosmetic/components/AppHeader.vue
@@ -1,3 +1,8 @@
 <template>
     <h2>AppHeader</h2>
 </template>
+<style scoped>
+    h2 {
+        color: blueviolet;
+    }
+</style>
--- frontend/cosmetic/nuxt.config.ts
+++ frontend/cosmetic/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            link: [
+                {
+                  rel: 'preconnect',
+                  href: 'https://fonts.googleapis.com'
+                },
+                {
+                  rel: 'stylesheet',
+                  href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+                  crossorigin: ''
+                },
+                {
+                    rel: 'stylesheet',
+                    href: '/styles/style.css',
+                    type: 'text/css'
+                }
+              ],
+        }
+    }
 })
