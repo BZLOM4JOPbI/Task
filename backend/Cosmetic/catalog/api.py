5b675ac9980ef97d6de579bc672a00f0fa1bc1c1
noise <boevdima779@gmail.com>
2023-04-14 19:17:43 +0300

    make post and get methods in API

--- backend/Cosmetic/catalog/api.py
+++ backend/Cosmetic/catalog/api.py
@@ -1,13 +1,38 @@
 from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import HttpResponse
 
 from catalog.serializers import *
 from catalog.models import *
 
 
-class ProductsViewSet(viewsets.ModelViewSet):
+# class ProductsViewSet(viewsets.ModelViewSet):
 
-    queryset = Products.objects.all()
-    permissions_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = ProductsSerializer
+#     queryset = Products.objects.all()
+#     permissions_classes = [
+#         permissions.AllowAny
+#     ]
+#     serializer_class = ProductsSerializer
+
+
+class ProductsAPiView(APIView):
+
+    def get(self, request):
+        
+        filters = request.query_params
+        print(filters)
+        products = Products.objects.all()
+        serializer = ProductsSerializer(products, many=True)
+
+        return Response({'data': serializer.data})
+
+    def post(self, request):
+
+        new_product = request.data
+        print(new_product)
+        serializer = ProductsSerializer(data=new_product)
+
+        if serializer.is_valid(raise_exception=True):
+            product_saved = serializer.save()
+        return Response({'Success': f'Товар {product_saved.title_of_product} успешно добавлен'})
--- backend/Cosmetic/catalog/urls.py
+++ backend/Cosmetic/catalog/urls.py
@@ -4,8 +4,13 @@ from rest_framework import routers
 from catalog.api import *
 
 
-router = routers.DefaultRouter()
-router.register('api/products', ProductsViewSet, 'products')
+# router = routers.DefaultRouter()
+# router.register('api/products', ProductsViewSet, 'products')
 
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+
+urlpatterns = [
+    path('api/products/', ProductsAPiView.as_view())
+]
